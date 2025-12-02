e1f0e5ae16b60635c8185619c5ed0d9b252ff613
I Gede Arya Danny Pratama <75374189+Gedearya@users.noreply.github.com>
2025-12-02 10:16:22 +0700

    Add error handling middleware in server.js
    Added error handling middleware for internal server errors.

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors'); // Import CORS
+const cors = require('cors'); 
 const connectDB = require('./db/connect');
 const studentScoresRoutes = require('./routes/studentScores');
 
-dotenv.config(); // Memuat variabel lingkungan dari file .env
+dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(express.json()); // Parsing JSON
-app.use(cors()); // Aktifkan CORS
+app.use(express.json()); 
+app.use(cors());
 
 connectDB(); // Koneksi ke database
 
 // Menggunakan routes untuk student scores
 app.use('/api/student-scores', studentScoresRoutes);
+
+app.use((req, res, next) => {
+  res.status(500).json({
+    message: "Internal Server Error"
+  });
+});
 
 // Menjalankan server
 const PORT = process.env.PORT || 5000;
